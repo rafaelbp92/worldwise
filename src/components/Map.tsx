5b686a78387b63c2b00451efbd024d82e2b5f31a
Rafael Pantoja <rafaelbp92@gmail.com>
2025-02-13 21:31:22 +0000

    Adding city markers to map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,9 +2,20 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { CitiesContextType, useCities } from "../contexts/CitiesContext";
+
+const formatDate = (date: string) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities() as CitiesContextType;
+
   const [mapPosition] = useState([49.2125, 28.4694]);
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
@@ -21,11 +32,18 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+              <span>{formatDate(city.date)}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
